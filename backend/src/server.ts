74991caf867fe5cc7f3d5cacd0fe2a30694b49a2
Douglas Vallinhos <douglas.m.vallinhos@gmail.com>
2020-10-16 22:31:00 -0300

    front e inico do backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { json } from 'express';
+import {getRepository} from 'typeorm';
+import Orphanage from  './models/Orphanage';
+
+import './database/connection';
+const app = express();
+app.use(express.json());
+
+app.post('/orphanages',async (req,res)=>{
+  const {name, latitude, longitude, about, instructions, opening_hours,open_on_weekends} = req.body;
+  const orphanagesRepository = getRepository(Orphanage);
+  const orphanage = orphanagesRepository.create({name, latitude, longitude, about, instructions, opening_hours,open_on_weekends});
+  await orphanagesRepository.save(orphanage);
+  return res.json({message: 'Orfanato cadastrado com sucesso!'})
+})
+app.listen(3333);
